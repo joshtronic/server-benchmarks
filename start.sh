a8c14ad4b4be757b355fd17d0f20485c9d706894
Josh Sherman <joshsherman@gmail.com>
2018-08-19 11:51:09 -0500

    fix: mysql benchmarks working again

--- start.sh
+++ start.sh
@@ -8,4 +8,5 @@ ssh "root@$2" "./run.sh $1"
 
 rsync -avz "root@$2:~/results-$1.tgz" ./
 
-ab -kc 1000 -n 10000 "http://$2/" > "./ab-$1.log"
+# ab -kc 1000 -n 10000 "http://$2/" > "./ab-$1.log"
+ab -kc 500 -n 10000 "http://$2/" > "./ab-$1.log"
