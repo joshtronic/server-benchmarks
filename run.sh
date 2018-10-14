ed583c464acac45d066a7b75dff09407a23b5b5e
Josh Sherman <joshsherman@gmail.com>
2018-10-14 11:17:14 -0500

    feat: conditional based on sysbench version

--- run.sh
+++ run.sh
@@ -13,6 +13,8 @@ apt-get update
 apt-get upgrade -y
 apt-get install sysbench nginx mysql-server python -y
 
+SYSBENCH_MAJOR_VERSION=$(sysbench --version | cut -d ' ' -f 2 | cut -d '.' -f 1)
+
 wget -O speedtest-cli https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py
 chmod +x speedtest-cli
 
@@ -29,10 +31,19 @@ sysbench --test=fileio prepare
 sysbench --test=fileio --file-test-mode=rndrw run > results/fileio.log
 sysbench --test=fileio cleanup
 
-mysql -uroot -e "CREATE DATABASE sbtest;"
-sysbench --db-driver=mysql --table-size=1000000 --mysql-user=root /usr/share/sysbench/oltp_read_write.lua prepare
-sysbench --db-driver=mysql --table-size=1000000 --mysql-user=root /usr/share/sysbench/oltp_read_write.lua run > results/mysql.log
-sysbench --db-driver=mysql --table-size=1000000 --mysql-user=root /usr/share/sysbench/oltp_read_write.lua cleanup
+if [ "$SYSBENCH_MAJOR_VERSION" == "0" ]; then
+  # Ubuntu 16.04
+  mysql -uroot -e "CREATE DATABASE sbtest;"
+  sysbench --test=oltp --oltp-table-size=1000000 --mysql-user=root prepare
+  sysbench --test=oltp --oltp-table-size=1000000 --mysql-user=root run > results/mysql.log
+  sysbench --test=oltp --oltp-table-size=1000000 --mysql-user=root cleanup
+else
+  # Ubuntu 18.04
+  mysql -uroot -e "CREATE DATABASE sbtest;"
+  sysbench --db-driver=mysql --table-size=1000000 --mysql-user=root /usr/share/sysbench/oltp_read_write.lua prepare
+  sysbench --db-driver=mysql --table-size=1000000 --mysql-user=root /usr/share/sysbench/oltp_read_write.lua run > results/mysql.log
+  sysbench --db-driver=mysql --table-size=1000000 --mysql-user=root /usr/share/sysbench/oltp_read_write.lua cleanup
+fi
 
 ./speedtest-cli --server=16089 > results/speedtest1.log
 ./speedtest-cli --server=16089 > results/speedtest2.log
