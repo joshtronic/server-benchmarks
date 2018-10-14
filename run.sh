7a8c5123a3deda4916c7bf108930438306b265b2
Josh Sherman <joshsherman@gmail.com>
2018-10-14 12:13:32 -0500

    chore: drop `ab` line entirely
    As I wrote out that last commit message I realized there's absolutely no reason
    to keep that around

--- run.sh
+++ run.sh
@@ -51,6 +51,4 @@ fi
 ./speedtest-cli --server=16089 > results/speedtest2.log
 ./speedtest-cli --server=16089 > results/speedtest3.log
 
-# ab -kc 1000 -n 10000 http://127.0.0.1/ > results/ab.log
-
 tar -zcvf "results-$PROVIDER.tgz" results
